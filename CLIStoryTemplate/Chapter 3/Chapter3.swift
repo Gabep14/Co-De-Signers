453f2e4ce5ac89a7fac51b33c375f9f9d6114b49
Fenton <313ironman@gmail.com>
2023-10-18 11:13:21 -0400

    Addkashdoll

--- CLIStoryTemplate/Chapter 3/Chapter3.swift
+++ CLIStoryTemplate/Chapter 3/Chapter3.swift
@@ -6,5 +6,12 @@
 import Foundation
 
 func chapterThree() {
-    // Your portion of the story goes here
+    print("Kash Doll born Mar 14 1992 and the oldest of six siblings")
+    print("Real name is Arkeisha Knight")
+    print("Drake contacts her via Instagram and invites her to participate in his tour. 2016")
+    print("She signs a record deal with Republic Records. 2019")
+    print("Her Acting Debut She lands a role in the television series Empire where she plays herself. 2021")
+    print("She appears in the series BMF as a recurring character. 2021")
+    print("Her Mixtape Drops After a brief hiatus, she releases her first Gangsta Grillz mixtape, Back On Dexter. 2023")
+    print("")
 }
