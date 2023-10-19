901346e9eff51d4741716dd43830af549d8cb2a1
Gabe <gabep@msu.idserve.net>
2023-10-19 11:55:25 -0400

    Update to Story

--- CLIStoryTemplate/Chapter 1/Chapter1.swift
+++ CLIStoryTemplate/Chapter 1/Chapter1.swift
@@ -5,9 +5,14 @@
 
 import Foundation
 
+let restuarant = "Mom's Spaghetti"
+let rapper = "Eminem"
+
+
 func chapterOne() {
-    print("42 Dugg was born in Detroit")
-    print("He was arrested and thrown in solitary confinement")
-    print("He learned to rap in jail and is now a very successful rapper")
+    print("\(rapper) was born in 1972 in Missouri, but quickly relocated to Detroit where he grew up.")
+    print("\(rapper) grew up to become one of the most popular rappers to come out of Detroit, and he gave a lot back to the city")
+    print("Not only did \(rapper) open a restuarant called \(restuarant), but he donated over $200,000 to the Detroit Childrens Charity, and donated to the Spartan Strong Foundation after the tragic events on MSU campus.")
+    print("Detroit rappers such as \(rapper) giving back to the city is a great thing to see, and inspires the next generation of Detroit to pursure their dreams.")
 }
 
