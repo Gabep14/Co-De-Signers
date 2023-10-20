40aeec951a7c182f53a03fd57f61122c1c575a86
Mya M <bornagoat1214@icloud.com>
2023-10-20 10:32:23 -0400

    Deleted Chapter 4

--- CLIStoryTemplate/Chapter 4/Chapter4.swift
+++ CLIStoryTemplate/Chapter 4/Chapter4.swift
@@ -6,6 +6,5 @@
 import Foundation
 
 func chapterFour() {
-    print("Danny Brown")
-    print("Once upon a time there was a boy named  Daniel Dewan Sewell who was born in Detroit MI. Everyday he would tell his parents he wanted to become a rapper, until one day at the age of 18 he got his first case for drug dealing. Because of that he told himself was going to take his rap career seriously. But he had another run in with the law, and because it violated his probation he ran and didn't go to court. Because of that he was on the run for at least 5 years. Until finally he got caught and did 8 months, he felt he had nothing else and started to go back to studying music to become a rapper. After his release from jail in 2002, he finally took his passion more seriously.  And ever since then he has been recording music with Detroit based producer Nick Speed.")
+    
 }
