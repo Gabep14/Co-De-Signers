aee7eed7a46774b5b3d5e755666bf95766a7d5fb
Rae650 <pricemaritza94@yahoo.com>
2023-10-23 21:41:50 -0400

    Chapter 4

--- CLIStoryTemplate/Chapter 2/Chapter2.swift
+++ CLIStoryTemplate/Chapter 2/Chapter2.swift
@@ -4,7 +4,10 @@
 //
 
 import Foundation
-
+let changeOfFate = 2005
+let character = "BigSean"
+let bigDate = 2005-1988
+let guild = "GOOD record label"
 func chapterTwo() {
-    // Your portion of the story goes here
+    "In";changeOfFate;character; "was";bigDate; "and was working as a telemarketer when he got a phone call from a friend who wanted to grant him a wish and make his dreams come true and introduce him to a Wizard " ;character; " went running for his chance but the Wizard wouldnt grant him his wish because he said" ;character; " wasnt good enough";character;"s";"friends pushed him to keep bugging the Wizard to give him a chance, finally the Wizard heard him out and let him show him his magic, the Wizard told him I see you can do great things with your talents and i will grant you your wish"; character;"gave the Wizard a book of his greatest magic and the Wizard asked him to become part of his" ;guild;
 }
