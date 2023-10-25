f132010e5bca9fa7810f6cb84968c6888bddf1a7
Gabe <gabep@msu.idserve.net>
2023-10-25 10:21:38 -0400

    Big Sean

--- CLIStoryTemplate/Chapter 1/Chapter1.swift
+++ CLIStoryTemplate/Chapter 1/Chapter1.swift
@@ -5,14 +5,21 @@
 
 import Foundation
 
-let restuarant = "Mom's Spaghetti"
-let rapper = "Eminem"
-
+let rapper = "Big Sean"
+let isHumble = false
 
 func chapterOne() {
-    print("\(rapper) was born in 1972 in Missouri, but quickly relocated to Detroit where he grew up.")
-    print("\(rapper) grew up to become one of the most popular rappers to come out of Detroit, and he gave a lot back to the city")
-    print("Not only did \(rapper) open a restuarant called \(restuarant), but he donated over $200,000 to the Detroit Childrens Charity, and donated to the Spartan Strong Foundation after the tragic events on MSU campus.")
-    print("Detroit rappers such as \(rapper) giving back to the city is a great thing to see, and inspires the next generation of Detroit to pursure their dreams.")
+    print("Big Sean: The Beginning")
+    print("Sean Michael Leonoard Anderson, known as \(rapper), was born in Detroit, Michigan, on March 25, 1988.")
+    print("\(rapper) was raised by his mother and grandparents to work hard in everything he did. He started rapping at the age of 12 at Waldorf School in Detroit, and would later attend Cass Tech High School, where he purued his music passion.")
+    print("\(rapper) even sold copies of his music in high school to his piers.")
+    print("Sean would rap occoasionaly over the Detroit radio station 102.7FM, where he met Kanye West. \(rapper) then worked hard to impress Kanye with freestyling and copies of his music, until one day \(rapper) finally got the call from Kanye to sign him.")
+    
+    if isHumble == true {
+        print("\(rapper) says: 'I was meant to inspire people, I was meant to make music.'  He also talks about his sacrifices in order to achieve his goals in an interview: 'There's been so many nights where instead of going out I've had to work and was depressed so I feel like I deserve this. This is what I was meant to do.' ")
+    }
+    else {
+        print("\(rapper) let the fame get to him, got involved with drugs and was thrown in jail.")
+    }
 }
-
+   
