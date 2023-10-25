b900165d1ca1e680ab2f098a1b71af86ce2ef989
Fenton <313ironman@gmail.com>
2023-10-25 11:20:10 -0400

    Chapter 3

--- CLIStoryTemplate/Chapter 3/Chapter3.swift
+++ CLIStoryTemplate/Chapter 3/Chapter3.swift
@@ -5,13 +5,34 @@
 
 import Foundation
 
-func chapterThree() {
-    print("Kash Doll born Mar 14 1992 and the oldest of six siblings")
-    print("Real name is Arkeisha Knight")
-    print("Drake contacts her via Instagram and invites her to participate in his tour. 2016")
-    print("She signs a record deal with Republic Records. 2019")
-    print("Her Acting Debut She lands a role in the television series Empire where she plays herself. 2021")
-    print("She appears in the series BMF as a recurring character. 2021")
-    print("Her Mixtape Drops After a brief hiatus, she releases her first Gangsta Grillz mixtape, Back On Dexter. 2023")
-    print("Detroit loves her")
+let character = "Big Sean"
+var movesToDetroit = false
+var staysInSantaMonica = false
+func endofchapter3(){print("Known professionally as \(character), is an American rapper")
+    print("Anderson began his music career in 2007 and gained popularity in 2010 with his third mixtape Finally Famous Vol 3 Big.")
+    print("\(character) then signed a recording contract with GOOD Music and Def Jam Recordings and released his debut studio album, Finally Famous in 2011, which included the US top 10 single Dance Ass featuring Nicki Minaj")
+    print("Anderson followed it with his second album, Hall of Fame 2013, while his third album, Dark Sky Paradise 2015, debuted atop the US Billboard 200.")
+    print("It also included the hit singles I Dont Fuck with You featuring E-40 and Blessings featuring Drake and Kanye West. His fourth album, I Decided 2017, contained Bounce Back, his highest-charting US single, while \(character) fifth album Detroit 2 2020 was his third consecutive platinum album.")}
+
+
+func chapterThree(){
+          print("SEAN ANDERSON FOUNDATION STUDIOS Multi-platinum artist,philanthropist and BGCSM Alumni \(character) was excited to return to Detroit for his 3rd Annual Detroits On Now D.O.N Weekend Saturday, August 28  Sunday,August 29")
+          print("The weekend was presented by the Sean Anderson Foundation (SAF) and hosted by the Boys & Girls Clubs of Southeastern Michigan (BGCSM) and City Council President Pro-Tem Mary Sheffield and offered free activities for all ages.")
+    
+    if movesToDetroit {
+        print (" Opens SEAN ANDERSON FOUNDATION STUDIOS as an adult")
+        
+    }
+
+    else if staysInSantaMonica {
+        print("Becomes a dishwasher at a resturant")
+        
+        endofchapter3()
+        
+    }
+    
+  
+    
 }
+    
+
