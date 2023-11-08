2e90ca9222070a57af7ab88bc97244d3de36195d
Fenton <313ironman@gmail.com>
2023-11-08 09:00:18 -0500

    final

--- CLIStoryTemplate/Chapter 3/Chapter3.swift
+++ CLIStoryTemplate/Chapter 3/Chapter3.swift
@@ -32,41 +32,56 @@ enum AlbumYear{
 }
 
 //functions aka func
-func endofchapter3(){
-   var i = 0
+func chapterThree(){
+    
+    //Optionals
+    
+    
+    var Charting: String? = "World Wide Fame"
+    
+    if let unwrappedCharting = Charting {
+        print(" \(Character.self) has some highest charting us single's and a lot of \(unwrappedCharting).")
+    } else {
+        print(" \(Character.self) did not chart.")
+        Charting = "Fame"
+    }
+    
+    print(Charting)
+    
+    
+    
+    // TODO: loop through platinum albums and display them
+    print("Known professionally as \(character), is an American rapper")
+    print("Anderson began his music career in 2007 and gained popularity in 2010 with his third mixtape Finally Famous Vol 3 Big.")
+    print("\(character) then signed a recording contract with GOOD Music and Def Jam Recordings and released his \(AlbumYear.album1) Finally Famous in 2011, which included the US top 10 single Dance Ass featuring Nicki Minaj")
+    print("Anderson followed it with his\(AlbumYear.album2) Hall of Fame 2013, while his \(AlbumYear.album3) Dark Sky Paradise 2015, debuted atop the US Billboard 200.")
+    print("It also included the hit singles I Dont Fuck with You featuring \(colab().E40) and Blessings featuring Drake and Kanye West. His \(AlbumYear.album4), I Decided 2017, contained Bounce Back, his highest-charting US single, while \(character) \(AlbumYear.album5) Detroit 2 2020 was his third consecutive platinum album.")
+    
+    var i = 0
     for _ in AlbumYear.platinumAlbums {
         print(AlbumYear.platinumAlbums[i])
         i += 1
     }
-        
     
-  // TODO: loop through platinum albums and display them
-   print("Known professionally as \(character), is an American rapper")
-    print("Anderson began his music career in 2007 and gained popularity in 2010 with his third mixtape Finally Famous Vol 3 Big.")
-    print("\(character) then signed a recording contract with GOOD Music and Def Jam Recordings and released his \(AlbumYear.album1) Finally Famous in 2011, which included the US top 10 single Dance Ass featuring Nicki Minaj")
-    print("Anderson followed it with his\(AlbumYear.album2) Hall of Fame 2013, while his \(AlbumYear.album3) Dark Sky Paradise 2015, debuted atop the US Billboard 200.")
-    print("It also included the hit singles I Dont Fuck with You featuring \(colab().E40) and Blessings featuring Drake and Kanye West. His \(AlbumYear.album4), I Decided 2017, contained Bounce Back, his highest-charting US single, while \(character) \(AlbumYear.album5) Detroit 2 2020 was his third consecutive platinum album.")}
-
-
-func chapterThree(){
-    print("SEAN ANDERSON FOUNDATION STUDIOS Multi-platinum artist,philanthropist and BGCSM Alumni \(character) was excited to return to Detroit for his 3rd Annual Detroits On Now D.O.N Weekend Saturday, August 28  Sunday,August 29")
-    print("The weekend was presented by the \(givingBack) and City Council President Pro-Tem Ma ry Sheffield and offered free activities for all ages.")
     
-    if movesToDetroit {
-        print (" Opens SEAN ANDERSON FOUNDATION STUDIOS as an adult")
+ 
+        print("SEAN ANDERSON FOUNDATION STUDIOS Multi-platinum artist,philanthropist and BGCSM Alumni \(character) was excited to return to Detroit for his 3rd Annual Detroits On Now D.O.N Weekend Saturday, August 28  Sunday,August 29")
+        print("The weekend was presented by the \(givingBack) and City Council President Pro-Tem Ma ry Sheffield and offered free activities for all ages.")
+    
+        if movesToDetroit {
+            print (" Opens SEAN ANDERSON FOUNDATION STUDIOS as an adult")
+    
+        }
+    
+        else if staysInSantaMonica {
+            print("Becomes a dishwasher at a resturant")
+    
+    
+    
+    
+        }
+
         
     }
     
-    else if staysInSantaMonica {
-        print("Becomes a dishwasher at a resturant")
-        
-
-        
-        endofchapter3()
-        
-    }
     
-}
-    
-
-
